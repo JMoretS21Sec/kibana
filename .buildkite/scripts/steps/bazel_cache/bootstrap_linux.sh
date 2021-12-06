e51f8a85c4be2200b3585891c409deae5ed30c9c
Brian Seeders <brian.seeders@elastic.co>
2021-12-06 15:59:23 -0500

    Add linux and ts refs

--- .buildkite/scripts/steps/bazel_cache/bootstrap_linux.sh
+++ .buildkite/scripts/steps/bazel_cache/bootstrap_linux.sh
@@ -7,8 +7,4 @@ export BUILD_TS_REFS_CACHE_CAPTURE=false # TODO
 export DISABLE_BOOTSTRAP_VALIDATION=true
 export BUILD_TS_REFS_DISABLE=false
 
-TMP_HOME="$WORKSPACE/tmp_home"
-rm -rf "$TMP_HOME"
-export HOME="$TMP_HOME"
-
 .buildkite/scripts/bootstrap.sh
